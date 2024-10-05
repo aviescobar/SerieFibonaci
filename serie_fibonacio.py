f011bbbfe0daaa251836b6633cfed317d733f782
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-04 22:27:04 -0600

    Update serie_fibonacio.py

--- serie_fibonacio.py
+++ serie_fibonacio.py
@@ -1,6 +1,7 @@
 def fibonacci(n):
   fib_sequence = [0, 1]
   for i in range(2, n):
-   next_fib = fib_sequence[-1] + fib_sequence[-2] 
-  fib_sequence.append(next_fib)
+      next_fib = fib_sequence[-1] + fib_sequence[-2] 
+      fib_sequence.append(next_fib)
+  return fib_sequence[:n]
   
